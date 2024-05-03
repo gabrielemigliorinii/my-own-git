2468a8c312288701a15c820c5c00f6cd2a3a8998
gabriele <gabrieledevs@gmail.com>
2024-05-03 20:09:14 +0200

    Add command checkout

--- mygitlib.py
+++ mygitlib.py
@@ -73,6 +73,14 @@ argsp.add_argument("-r", dest="recursive", action="store_true", help="Recurse in
 argsp.add_argument("tree", help="A tree-ish object.")
 
 
+
+argsp = argsubparsers.add_parser("checkout", help="Checkout a commit inside of a directory.")
+
+argsp.add_argument("commit", help="The commit or tree to checkout.")
+
+argsp.add_argument("path", help="The EMPTY directory to checkout on.")
+
+
 # ------------------------------------------------------------------------------------
 
 
@@ -138,6 +146,29 @@ def cmd_log(args):
 def cmd_ls_tree(args):
     repo = repo_find()
     ls_tree(repo, args.tree, args.recursive)
+
+
+def cmd_checkout(args):
+
+    repo = repo_find()
+
+    obj = object_read(repo, object_find(repo, args.commit))
+
+    # If the object is a commit, we grab its tree
+    if obj.fmt == b'commit':
+        obj = object_read(repo, obj.commit[b'tree'].decode("ascii"))
+
+    # Verify that path is an empty directory
+    if os.path.exists(args.path):
+
+        if not os.path.isdir(args.path):
+            raise Exception("Not a directory {0}!".format(args.path))
+        if os.listdir(args.path):
+            raise Exception("Not empty {0}!".format(args.path))
+    else:
+        os.makedirs(args.path)
+
+    tree_checkout(repo, obj, os.path.realpath(args.path))
 
 
 # End bridge functions
@@ -694,6 +725,23 @@ def ls_tree(repo, ref, recursive=None, prefix=""):
             ls_tree(repo, item.sha, recursive, os.path.join(prefix, item.path))
 
 
+def tree_checkout(repo, tree, path):
+
+    for item in tree.items:
+        
+        obj = object_read(repo, item.sha)
+        dest = os.path.join(path, item.path)
+
+        if obj.fmt == b'tree':
+            
+            os.mkdir(dest)
+            tree_checkout(repo, obj, dest)
+        
+        elif obj.fmt == b'blob':
+            
+            # @TODO Support symlinks (identified by mode 12****)
+            with open(dest, 'wb') as f:
+                f.write(obj.blobdata)
 
 
 # End utilities
